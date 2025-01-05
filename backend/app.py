fb094ea4c2dd6b706c9d7ca5b27b65674a5a0b74
BAGAS SEBASTIAN <111202214715@mhs.dinus.ac.id>
2025-01-05 08:25:31 +0700

    update model, nodejs, and encrypt

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import encrypt
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from joblib import load
@@ -33,14 +34,15 @@ def extact_feature(ciphertext):
     return features
 
 def decrypt_caesar(ciphertext):
-    model = load('predict_shift.joblib')
+    model = load('new_model.joblib')
     feature = np.array([extact_feature(ciphertext)])
     key = model.predict(feature)[0]
-    return encrypt_caesar(ciphertext, -key)
+    return encrypt_caesar(ciphertext, -key), key
     # key = model.predict(ciphertext)
 
 def parse_input(req: str) ->str:
-    cleaned_string = re.sub(r'[0-9\s,.!@#\$%\^\&*\)\(+=_-]', '', req)
+    cleaned_string = req.lower()
+    cleaned_string = re.sub(r'[0-9\s,.!@#\$%\^\&*\)\(+=_-]', '', cleaned_string)
     print('cleaned string:\t',cleaned_string)
     return cleaned_string
 
@@ -51,11 +53,23 @@ def caesar():
    req = request.get_json()
    req['cipher'] = parse_input(req['cipher'])
    
+   result, key = decrypt_caesar(req['cipher'])
    response_data = {}
-   response_data['plaintext'] = decrypt_caesar(req['cipher'])
-   response_data['frequency'] = extact_feature(req['cipher'])
+   response_data['plaintext'] = result
+   response_data['key'] = key
+   response_data['cipher_frequency'] = extact_feature(req['cipher'])
+   response_data['plain_frequency'] = extact_feature(result)
    return response_data
-   
+
+@app.route('/encrypt', methods=['POST'])
+@cross_origin
+def encrypt():
+    req = request.get_json()
+    if not req['plaintext'] or not req['shift']:
+        return jsonify({"error":
+                        "input plaintext atau shift tidak boleh kosong"
+                        }), 400 
+    return encrypt_caesar(str(req['plaintext']), int(req["shift"]))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
